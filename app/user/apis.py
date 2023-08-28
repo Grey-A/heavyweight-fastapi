3660b0e251977dfe7a16f4c39694f3066f346d70
Bello Ango <angobello0@gmail.com>
2023-08-28 23:22:18 +0100

    implemented create_user endpoint

--- app/user/apis.py
+++ app/user/apis.py
@@ -1,3 +1,17 @@
-from fastapi import APIRouter
+from fastapi import status, APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.dependencies import get_db
+
+from app.user import schemas, services
 
 router = APIRouter()
+
+
+@router.post(
+    "/create",
+    status_code=status.HTTP_201_CREATED,
+    response_model=schemas.User,
+)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # Directly call the service function to create a new user
+    return services.create_contributor(user=user, db=db)
