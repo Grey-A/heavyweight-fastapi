f40a125e22aea5ab555b2b8e2317e6bec7b6467b
Bello Ango <angobello0@gmail.com>
2023-08-28 23:04:49 +0100

    created settings.py

---
+++ app/config/settings.py
@@ -0,0 +1,28 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # Security
+    SECRET_KEY: str = os.environ.get("SECRET_KEY")
+    HASHING_ALGORITHM: str = os.environ.get("HASHING_ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+    # DB Settings
+    POSTGRES_USER: str = os.environ.get("PGUSER")
+    POSTGRES_PASSWORD: str = os.environ.get("PGPASSWORD")
+    POSTGRES_SERVER: str = os.environ.get("PGHOST")
+    POSTGRES_PORT: int = os.environ.get("PGPORT")
+    POSTGRES_DB: str = os.environ.get("PGDATABASE")
+
+    class Config:
+        env_file = ".env"
+
+    @property
+    def POSTGRES_URL(self):
+        url = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return url
+
+
+settings = Settings()
