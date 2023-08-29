0f69d7b5ad140dc2a393c03684e0911b2d5ce3a4
Bello Ango <angobello0@gmail.com>
2023-08-29 15:45:12 +0100

    created login and user detail endpoints

--- app/user/apis.py
+++ app/user/apis.py
@@ -1,7 +1,7 @@
 from fastapi import status, APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.dependencies import get_db
 
+from app.dependencies import get_db
 from app.user import schemas, services
 
 router = APIRouter()
@@ -15,3 +15,23 @@ router = APIRouter()
 def user_create(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # Directly call the service function to create a new user
     return services.create_user(user=user, db=db)
+
+
+@router.post(
+    "/login",
+    status_code=status.HTTP_200_OK,
+    response_model=schemas.Token,
+)
+def user_login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    # Directly call the service function to login
+    return services.login_user(user_credentials=user, db=db)
+
+
+@router.get(
+    "/me",
+    status_code=status.HTTP_200_OK,
+    response_model=schemas.User,
+)
+def user_details(user: schemas.User = Depends(services.get_current_user)):
+    # Directly return the user obj
+    return user
