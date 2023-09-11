24b97af22cb112aefd46b0f9dc7794e476eefd3e
Bello Ango <angobello0@gmail.com>
2023-09-11 10:23:15 +0100

    changed doc url

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.user.apis import router as user_router
 
-app = FastAPI(title="Heavyweight(FastAPI)")
+app = FastAPI(title="Heavyweight(FastAPI)", docs_url="/")
 
 
 # Routers
