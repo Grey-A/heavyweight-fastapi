77389749f80a1ab41c943074ef28d772f10c8e9f
Bello Ango <angobello0@gmail.com>
2023-10-14 12:31:14 +0100

    bug fix

--- app/user/selectors.py
+++ app/user/selectors.py
@@ -1,3 +1,4 @@
+from fastapi import status, HTTPException
 from sqlalchemy.orm import Session
 
 from app.user import models
@@ -13,4 +14,7 @@ def get_user(email: str, db: Session):
     Returns:
         models.User: The user obj
     """
-    return db.query(models.User).filter_by(email=email).first()
+    user = db.query(models.User).filter_by(email=email).first()
+    if user and user.is_active:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
