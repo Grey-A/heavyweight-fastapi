fb36a77b355b95aa1186024a9b7fc955f36c4471
Bello Ango <angobello0@gmail.com>
2024-04-04 14:38:10 +0100

    db: switch to new get_settings() and add extra db configs

--- alembic/env.py
+++ alembic/env.py
@@ -5,12 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app.config.settings import settings
+from app.config.settings import get_settings
+
+settings = get_settings()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.POSTGRES_URL)
+config.set_main_option("sqlalchemy.url", settings.POSTGRES_DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
--- app/config/database.py
+++ app/config/database.py
@@ -2,10 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from app.config.settings import settings
+from app.config.settings import get_settings
+
+settings = get_settings()
 
 engine = create_engine(
-    url=settings.POSTGRES_URL,
+    url=settings.POSTGRES_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=100,  # The size of the connection pool
+    max_overflow=50,  # The maximum number of connections that can be opened beyond the pool size. Set to -1 for no limit.
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
