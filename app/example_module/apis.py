303035e2a9f74871a963f7cfddcc2bde3ee05c5e
Bello Ango <angobello0@gmail.com>
2024-04-04 15:58:42 +0100

    app: performance improvements based on https://youtu.be/7jtzjovKQ8A?si=zmcpFj4FLMGApwoF

--- app/example_module/apis.py
+++ app/example_module/apis.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 
 @router.get("/", summary="Example Endpoint", response_model=schemas.TheMessage)
-def example_endpoint():
+async def example_endpoint():
     return schemas.TheMessage(
         content="Accepting the Mandate of Heaven, May the People Live Long and Railway Forever Prosper"
     )
--- app/main.py
+++ app/main.py
@@ -1,19 +1,43 @@
+from anyio import to_thread
+from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI
+from fastapi.responses import ORJSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 from app.dependencies import get_db
 from app.example_module.apis import router as example_router
 
+
+# Lifespan (startup, shutdown)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup code
+    print("System Call: Enhance Armament x_x")  # SAO Reference
+
+    # Bigger Threadpool i.e you send a bunch of requests it will handle a max of 1000 at a time, the default is 40
+    limiter = to_thread.current_default_thread_limiter()
+    limiter.total_tokens = 1000
+
+    # Shutdown
+    yield
+    print("System Call: Release Recollection...")
+
+
 app = FastAPI(
     title="Heavyweight(FastAPI)",
     docs_url="/",
-    swagger_ui_parameters={"defaultModelsExpandDepth": -1}, # Hides Schemas Menu in DocsF
+    swagger_ui_parameters={
+        "defaultModelsExpandDepth": -1
+    },  # Hides Schemas Menu in Docs
+    lifespan=lifespan,
+    default_response_class=ORJSONResponse,
 )
 
 # Variables
 origins = ["*"]
 
-# Middleware
+# Middlewares
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -21,11 +45,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(
+    GZipMiddleware,
+    minimum_size=5000,  # Minimum size of the response before it is compressed in bytes
+)
 
 
 # Health Check
 @app.get("/health", status_code=200, include_in_schema=False)
-def health_check(db=Depends(get_db)):
+async def health_check(db=Depends(get_db)):
     """This is the health check endpoint"""
     return {"status": "ok"}
 
