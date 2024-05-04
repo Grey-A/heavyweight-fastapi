24f10bb916721d97ef055ac5dcc859c2eff876ac
Bello Ango <angobello0@gmail.com>
2024-05-04 14:37:47 +0100

    update: switched declarative_base import to sqlalchemy.orm

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from app.config.settings import get_settings
 
